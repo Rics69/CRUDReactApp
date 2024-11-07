a85e95b219d021968e233f189f912f01062ccdc7
Rics <danil.klementev.90@gmail.com>
2024-11-08 00:27:17 +0300

    add counter for component app-info and update all components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,32 +1,88 @@
-import './app.css';
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employess-add-form/employees-add-form';
 
-function App() {
+import './app.css';
 
-    const data = [
-        {name: 'Danil K.', salary: '1000', increase: true, id: 1},
-        {name: 'Yarik K.', salary: '300', increase: false, id: 2},
-        {name: 'Artur H.', salary: '900', increase: true, id: 3}
-    ];
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Danil K.', salary: '1000', increase: true, rise: true, id: 1},
+                {name: 'Yarik K.', salary: '300', increase: false, rise: false, id: 2},
+                {name: 'Artur H.', salary: '900', increase: true, rise: false, id: 3}
+            ]
+        }
 
-    return (
-        <div className="app">
-            <AppInfo/>
+        this.currentId = 4;
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            // const index = data.findIndex(elem => elem.id === id);
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        });
+    }
+
+    onAdd = (name, salary) => {
+        const newEmp = {
+            name: name,
+            salary: salary,
+            increase: false,
+            rise: false,
+            id: this.currentId++
+        };
+
+        this.setState(({data}) => {
+            const newData = [...data, newEmp];
+            return {
+                data: newData
+            }
+        });
+    }
+
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id){
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+    
+    render() {
+        const {data} = this.state;
+        const countIncrease = data.filter(item => item.increase).length;
+
+        return (
+            <div className="app">
+                <AppInfo 
+                allEmp={data.length}
+                allIncrease={countIncrease}/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployeesList 
+                data={data}
+                onDelete={this.deleteItem}
+                onToggleProp={this.onToggleProp}/>
+    
+                <EmployeesAddForm onAdd={this.onAdd}/>
             </div>
-
-            <EmployeesList data={data}/>
-
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,61 +1,37 @@
-import { Component } from 'react';
-
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            like: false
-        }
+const EmployeesListItem = ({name, salary, onDelete, onToggleProp, increase, rise}) => {
+    let curClass = "list-group-item d-flex justify-content-between";
+
+    if (increase){
+        curClass += " increase";
     }
-
-    changeIncreaseState = () => {
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
-
-    changeLikeState = (e) => {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
-
-    render() {
-        const {name, salary} = this.props;
-        const {increase, like} = this.state;
-        let curClass = "list-group-item d-flex justify-content-between";
-
-        if (increase){
-            curClass += " increase";
-        } 
-        
-        if (like){
-            curClass += " like";
-        }
-
-        return (
-            <li className={curClass}>
-                <span className="list-group-item-label" onClick={this.changeLikeState}>{name}</span>
-                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-                <div className='d-flex justify-content-center align-items-center'>
-                    <button type="button"
-                        className="btn-cookie btn-sm "
-                        onClick={this.changeIncreaseState}>
-                        <i className="fas fa-cookie"></i>
-                    </button>
     
-                    <button type="button"
-                            className="btn-trash btn-sm ">
-                        <i className="fas fa-trash"></i>
-                    </button>
-                    <i className="fas fa-star"></i>
-                </div>
-            </li>
-        );
+    if (rise){
+        curClass += " like";
     }
+
+    return (
+        <li className={curClass}>
+            <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise">{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+            <div className='d-flex justify-content-center align-items-center'>
+                <button type="button"
+                    className="btn-cookie btn-sm "
+                    onClick={onToggleProp}
+                    data-toggle="increase">
+                    <i className="fas fa-cookie"></i>
+                </button>
+
+                <button type="button"
+                        className="btn-trash btn-sm "
+                        onClick={onDelete}>
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    );
 }
 
 export default EmployeesListItem;
